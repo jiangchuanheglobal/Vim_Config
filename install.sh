75c322767d6006039f511b47bc5c372394a173e5
jiangchuan he <jhe@idirect.net>
2016-11-20 19:42:54 -0500

    Minor fix

--- install.sh
+++ install.sh
@@ -4,4 +4,4 @@ set -e
 
 vim_config=$(pwd)
 ln -s "$vim_config" ~/.vim
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+git clone git@github.com:VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
